1794598655fc4f801ad68359661a026f6d0591b1
latttysh <semyon.latysh@gmail.com>
2022-09-03 11:18:32 +0300

    +

---
+++ src/wallet/wallet.model.ts
@@ -0,0 +1,14 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+
+@Table({tableName: "wallet"})
+export class Wallet extends Model{
+  @Column({type: DataType.INTEGER, unique: true, autoIncrement:true, primaryKey: true})
+  id: number;
+
+  @Column({type: DataType.STRING, unique: false, allowNull: true})
+  name: string;
+
+  @Column({type: DataType.STRING, unique: false, allowNull: true})
+  address: string;
+}
